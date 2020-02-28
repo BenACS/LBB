789a4671f4e4a949e391a7b5a3ad9974bc474f8e
Benjamin.Cholvin <cholvin.benjamin@gmail.com>
2020-02-28 15:14:10 +0000

    Working on carousel & getting products on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Product;
+use App\Entity\ArticleImages;
+
 class HomeController extends AbstractController
 {
     /**
@@ -12,8 +15,16 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+    	$em = $this->getDoctrine()->getManager();
+    	$products = $em->getRepository(Product::class)->findAll();
+    	$articleImages = $em->getRepository(ArticleImages::class)->findAll();
+
+    	dump($articleImages);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products,
+            'articleImages' => $articleImages
         ]);
     }
 }
