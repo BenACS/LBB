3d856b6233b9c14e9f74d0056f3bf9c73ddba7c2
Benjamin.Cholvin <cholvin.benjamin@gmail.com>
2020-02-28 14:36:39 +0000

    Enabled jquery through app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,6 @@ import '../css/app.css';
 import "bootstrap";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
