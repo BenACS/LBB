f0c13a82b06e142abf807f72f3f4b23f8c208d29
Kévin Huonic <kevininc155@gmail.com>
2020-02-28 10:41:50 +0000

    rr

--- src/Migrations/Version20200228103523.php
+++ src/Migrations/Version20200228103523.php
@@ -21,25 +21,24 @@ final class Version20200228103523 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('INSERT INTO `article` (`id`, `product_id`, `stock`, `size`, `color`, `device`, `creation_date`) VALUES (1, 1, 25, NULL, NULL, NULL, "2020-02-28 10:36:00"),  (2, 2, 25, NULL, NULL, NULL, "2020-02-28 10:37:00"),
-        (3, 3, 10, NULL, NULL, NULL, "2020-02-28 10:41:00"),
-        (4, 4, 10, NULL, NULL, "iPhone 6/7/8", "2020-02-28 10:42:00"),
-        (5, 4, 10, NULL, NULL, "Samsung Galaxy S8/S9", "2020-02-28 10:42:00"),
-        (6, 5, 25, NULL, NULL, NULL, "2020-02-28 10:47:00"),
-        (7, 6, 10, "37/40", "Black", NULL, "2020-02-28 10:52:00"),
-        (8, 6, 10, "40/43", "Black", NULL, "2020-02-28 10:52:00"),
-        (9, 7, 10, "S", "Black", NULL, "2020-02-28 10:52:00"),
-        (10, 7, 10, "M", "Black", NULL, "2020-02-28 10:52:00"),
-        (11, 7, 10, "L", "Black", NULL, "2020-02-28 10:52:00"),
-        (12, 7, 10, "XL", "Black", NULL, "2020-02-28 10:52:00"),
-        (13, 8, 10, "S", "White", NULL, "2020-02-28 10:52:00"),
-        (14, 8, 10, "M", "White", NULL, "2020-02-28 10:52:00"),
-        (15, 8, 10, "L", "White", NULL, "2020-02-28 10:52:00"),
-        (16, 8, 10, "XL", "White", NULL, "2020-02-28 10:52:00"),
-        (17, 8, 10, "S", "Black", NULL, "2020-02-28 10:52:00"),
-        (18, 8, 10, "M", "Black", NULL, "2020-02-28 10:52:00"),
-        (19, 8, 10, "L", "Black", NULL, "2020-02-28 10:52:00"),
-        (20, 8, 10, "XL", "Black", NULL, "2020-02-28 10:52:00")') ;
+        $this->addSql("INSERT INTO `category` (`id`, `category_name`, `parent_id`) VALUES
+        (1, 'Clothes', 0),
+        (2, 'Accessories', 0),
+        (3, 'Mugs', 0),
+        (4, 'Surprises', 0),
+        (5, 'Classic', 3),
+        (6, 'Thermic', 3),
+        (7, 'T-shirts', 1),
+        (8, 'Hoodies', 1),
+        (9, 'Underwear', 1),
+        (10, 'Totebag', 2),
+        (11, 'Beanie', 2),
+        (12, 'Phone Case', 2),
+        (13, 'Socks', 2),
+        (14, 'Gourd', 2),
+        (15, 'Books', 4),
+        (16, 'Scarfs', 2)");
+        
     }
 
     public function down(Schema $schema) : void
