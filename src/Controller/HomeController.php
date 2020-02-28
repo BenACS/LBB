3397a4445e508ee957a78178f84bebc0d03debf0
Benjamin.Cholvin <cholvin.benjamin@gmail.com>
2020-02-28 15:35:55 +0000

    Working on carousel & can get products, articleImages, Categories in homepage. Navbar gets categories with parentId 0 for buttons.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\ArticleImages;
 
@@ -16,13 +17,15 @@ class HomeController extends AbstractController
     public function index()
     {
     	$em = $this->getDoctrine()->getManager();
+    	$categories = $em->getRepository(Category::class)->findAll();
     	$products = $em->getRepository(Product::class)->findAll();
     	$articleImages = $em->getRepository(ArticleImages::class)->findAll();
 
-    	dump($articleImages);
+    	dump($categories);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'categories' => $categories,
             'products' => $products,
             'articleImages' => $articleImages
         ]);
