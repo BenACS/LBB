12e67c4564a51185cf1cba7d97085de753f72f81
MCStrigl <m.strigl.acs@gmail.com>
2020-03-03 10:25:09 +0000

    ajout dans product.js + ajout commande pour watch correctement

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,1 +1,13 @@
-console.log("Hello losers");
+let thumbnails = document.getElementsByClassName("thumbnail-images");
+
+for (let i=0 ; i<thumbnails.length ; i++) {
+    thumbnails[i].onmouseover = function() {
+        thumbnails[i].classList.add('border-secondary');
+        main_image.src = thumbnails[i].src;
+    }
+    thumbnails[i].onmouseout = function() {
+        thumbnails[i].classList.remove('border-secondary');
+    }
+}
+
+console.log("hello");
