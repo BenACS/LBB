b99b4495abdecbac0bfa331bf0260cae83f668bc
MCStrigl <m.strigl.acs@gmail.com>
2020-03-03 13:30:17 +0000

    ajout fonction changement couleur image en fonction du filtre choisi

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,13 +1,33 @@
 let thumbnails = document.getElementsByClassName("thumbnail-images");
 
+
 for (let i=0 ; i<thumbnails.length ; i++) {
     thumbnails[i].onmouseover = function() {
+        for (thumbnail of thumbnails) {
+            thumbnail.classList.remove('border-secondary');
+        }
         thumbnails[i].classList.add('border-secondary');
         main_image.src = thumbnails[i].src;
     }
-    thumbnails[i].onmouseout = function() {
-        thumbnails[i].classList.remove('border-secondary');
+}
+
+if (document.getElementById("color_selector")) {
+    
+    for (let i=0 ; i<color_selector.length ; i++) {
+        changeImageDependingOnColor(color_selector[i]);
+        color_selector[i].onclick = function() {
+            changeImageDependingOnColor(color_selector[i]);
+        }
     }
 }
 
-console.log("hello");
+
+function changeImageDependingOnColor(element) {
+    for (thumbnail of thumbnails) {
+        thumbnail.classList.remove('border-secondary');
+        if (thumbnail.src.includes(element.value.toLowerCase())) {
+            main_image.src = thumbnail.src;
+            thumbnail.classList.add('border-secondary');
+        }
+    }
+}
