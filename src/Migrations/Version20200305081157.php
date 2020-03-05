38e1d74e87f2a76bd7c0d30a66fefcc20bb4fa43
Kévin Huonic <kevininc155@gmail.com>
2020-03-05 10:50:39 +0000

    modif migration

--- src/Migrations/Version20200305081157.php
+++ src/Migrations/Version20200305081157.php
@@ -12,40 +12,42 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200305081157 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE category ADD image VARCHAR(255) DEFAULT NULL');
 
-        $imageLinks = [NULL, NULL, NULL, NULL, 'https://i.ibb.co/7XmfDJS/classic-mugs.jpg',
-            'https://i.ibb.co/vD3hRwD/thermic-mug.jpg',
-            'https://i.ibb.co/4W1DDqW/t-shirts.jpg',
-            'https://i.ibb.co/5Y4Grwb/hoodies.jpg',
-            'https://i.ibb.co/DM2RxdR/underwears.jpg',
-            'https://i.ibb.co/vDvjYSS/totebags.jpg',
-            'https://i.ibb.co/1TWd9X8/beanies.jpg',
-            'https://i.ibb.co/xzGjmrg/phone-cases.jpg',
-            'https://i.ibb.co/74Y21T7/socks.jpg',
-            'https://i.ibb.co/8smg96K/gourds.jpg',
-            'https://i.ibb.co/5RrcyQp/books.jpg',
-            'https://i.ibb.co/C0kdXGr/scarfs.jpg',
-            NULL,
-            'https://i.ibb.co/Jx3q4MK/full-stack.jpg'];
-            
-            for ($i = 0 ; $i < count($imageLinks); $i++) {
-                $idvalue = $i+1;
-                $this->addSql("UPDATE category  SET `image` = $imageLinks[$i] WHERE id = $idvalue");
-            }
+        $imageLinks = [
+            "NULL", "NULL", "NULL", "NULL", '"https://i.ibb.co/7XmfDJS/classic-mugs.jpg"',
+            '"https://i.ibb.co/vD3hRwD/thermic-mug.jpg"',
+            '"https://i.ibb.co/4W1DDqW/t-shirts.jpg"',
+            '"https://i.ibb.co/5Y4Grwb/hoodies.jpg"',
+            '"https://i.ibb.co/DM2RxdR/underwears.jpg"',
+            '"https://i.ibb.co/vDvjYSS/totebags.jpg"',
+            '"https://i.ibb.co/1TWd9X8/beanies.jpg"',
+            '"https://i.ibb.co/xzGjmrg/phone-cases.jpg"',
+            '"https://i.ibb.co/74Y21T7/socks.jpg"',
+            '"https://i.ibb.co/8smg96K/gourds.jpg"',
+            '"https://i.ibb.co/5RrcyQp/books.jpg"',
+            '"https://i.ibb.co/C0kdXGr/scarfs.jpg"',
+            "NULL",
+            '"https://i.ibb.co/Jx3q4MK/full-stack.jpg"'
+        ];
+
+        for ($i = 0; $i < count($imageLinks); $i++) {
+            $idvalue = $i + 1;
+            $this->addSql("UPDATE category  SET `image` = $imageLinks[$i] WHERE id = $idvalue");
+        }
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
