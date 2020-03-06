7108ccd301ab3cf8c16380ab7c94dfc1fcc9c4e7
C.Ben <30369625+BenACS@users.noreply.github.com>
2020-03-06 09:17:15 +0100

    Homepage OK. URLs : à terminer.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,39 +7,21 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Category;
-use App\Entity\Product;
-use App\Entity\ArticleImages;
-use App\Entity\Article;
+use App\Repository\CategoryRepository;
 
 use App\Service\Header\HeaderService;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/{category}", name="category", requirements={"category": "^(?!product).+"})
+     * @Route("/{category}", name="category")
      */
     public function index(HeaderService $cat)
     {
-        $em = $this->getDoctrine()->getManager();
 
-        $products = $em->getRepository(Product::class)->findAll();
-        $article = $em->getRepository(Article::class)->findAll();
-        $articleImages = $em->getRepository(ArticleImages::class)->findAll();
-
-        // Lets us filter for the URLs
-        foreach ( $products as $product) {
-            foreach ( $product -> getArticles() as $article) {
-                foreach ( $article -> getArticleImages() as $image) {
-                    if (!isset ($images) || !in_array($image->getUrl(),$images)) {
-                        $images[] = $image->getUrl();
-                    }
-                }
-            }
-        }
-        
         return $this->render('category/index.html.twig', [
-            'articleImages' => $images,
-            'categories' => $cat->createHeader()
+            'categories' => $cat->createHeader(),
+            'test' => $cat->getMainCategoriesName()
         ]);
         
     }
