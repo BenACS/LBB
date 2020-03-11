bed886c7ed2551248aa49784a2c3c044f37b1d25
Kévin Huonic <kevininc155@gmail.com>
2020-03-11 07:34:09 +0000

    recuperation de l'user avec security

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Adress;
+use App\Entity\Account;
 use App\Form\AdressType;
 use App\Service\Header\HeaderService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,10 +15,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfilController extends AbstractController
 {
     private $session;
+    private $security;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, Security $security)
     {
         $this->session = $session;
+        $this->security = $security;
     }
     /**
      * @Route("/profil", name="profil")
@@ -24,14 +28,16 @@ class ProfilController extends AbstractController
     public function index(HeaderService $header, Request $request)
     {
         $manager = $this->getDoctrine()->getManager();
+        $userId = $this->security->getUser();
+        // dd($userId);
         $userAdress = new Adress();
-        dd($this->session);
+
         $form = $this->createForm(AdressType::class, $userAdress);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // $userAdress->setAccount($this->session);
+            $userAdress->setAccount($userId);
             $manager->persist($userAdress);
             $manager->flush();
 
