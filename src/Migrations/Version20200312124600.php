0d86a5c55cb4879967c287d49a4b70f693e4bfd0
William.Vilela <willivilela@gmail.com>
2020-03-12 13:06:04 +0000

    test

--- src/Migrations/Version20200312124600.php
+++ src/Migrations/Version20200312124600.php
@@ -22,7 +22,7 @@ final class Version20200312124600 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE account DROP role');
+        $this->addSql('ALTER TABLE account DROP roles');
         $this->addSql('ALTER TABLE account ADD roles JSON DEFAULT NULL, CHANGE register_date register_date DATETIME NOT NULL');
     }
 
