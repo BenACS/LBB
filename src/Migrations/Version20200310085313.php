37d75a7a80d19949dffd8d8cd41b58df377ad2cc
C.Ben <30369625+BenACS@users.noreply.github.com>
2020-03-12 08:41:22 +0100

    Maj migration

--- src/Migrations/Version20200310085313.php
+++ src/Migrations/Version20200310085313.php
@@ -21,8 +21,6 @@ final class Version20200310085313 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE CHANGE newsletter newsletter TINYINT(1) DEFAULT \'0\' NULL, CHANGE register_date register_date DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
